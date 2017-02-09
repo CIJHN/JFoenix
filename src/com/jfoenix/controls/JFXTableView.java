05b3c0d8568c83305fa8fbad24008dc33540f1fe
ci010 <cijhn@hotmail.com>
2017-02-08 17:54:42 -0800

    fix the table view fatal bug...

--- src/com/jfoenix/controls/JFXTableView.java
+++ src/com/jfoenix/controls/JFXTableView.java
@@ -20,156 +20,191 @@ import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 import java.util.TreeMap;
 import java.util.function.Predicate;
 
 /**
  * @author ci010
  */
-public class JFXTableView<S> extends TableView<S> {
-    private ObservableList<S> itemSource;
+public class JFXTableView<S> extends TableView<S>
+{
+	private FilteredList<S> itemSource;
 
-    public JFXTableView() {
-        this(FXCollections.observableArrayList());
-        init();
-    }
+	public JFXTableView()
+	{
+		super(FXCollections.observableArrayList());
+		this.itemSource = new FilteredList<>(FXCollections.observableArrayList());
+		init();
+	}
 
-    public JFXTableView(ObservableList<S> items) {
-        super(items);
-        this.itemSource = items;
-        init();
-    }
+	public ObservableList<S> getItemSource() {return itemSource;}
 
-    private BooleanProperty columnsDraggable = new SimpleBooleanProperty(true);
+	public void setItemSource(ObservableList<S> itemSource)
+	{
+		Objects.requireNonNull(itemSource);
+		this.itemSource = new FilteredList<>(itemSource);
+	}
 
-    public boolean isColumnsDraggable() {
-        return columnsDraggable.get();
-    }
+	private BooleanProperty columnsDraggable = new SimpleBooleanProperty(true);
 
-    public BooleanProperty columnsDraggableProperty() {
-        return columnsDraggable;
-    }
+	public boolean isColumnsDraggable()
+	{
+		return columnsDraggable.get();
+	}
 
-    public void setColumnsDraggable(boolean columnsDraggable) {
-        this.columnsDraggable.set(columnsDraggable);
-    }
+	public BooleanProperty columnsDraggableProperty()
+	{
+		return columnsDraggable;
+	}
 
-    private BooleanProperty fixedSize = new SimpleBooleanProperty(false);
+	public void setColumnsDraggable(boolean columnsDraggable)
+	{
+		this.columnsDraggable.set(columnsDraggable);
+	}
 
-    public boolean isFixedSize() {
-        return fixedSize.get();
-    }
+	private BooleanProperty fixedSize = new SimpleBooleanProperty(false);
 
-    public BooleanProperty fixedSizeProperty() {
-        return fixedSize;
-    }
+	public boolean isFixedSize()
+	{
+		return fixedSize.get();
+	}
 
-    public void setFixedSize(boolean fixedSize) {
-        this.fixedSize.set(fixedSize);
-    }
+	public BooleanProperty fixedSizeProperty()
+	{
+		return fixedSize;
+	}
 
-    private MapProperty<String, Predicate<S>> filterMap = new SimpleMapProperty<>(FXCollections.observableMap(new TreeMap<String, Predicate<S>>()));
+	public void setFixedSize(boolean fixedSize)
+	{
+		this.fixedSize.set(fixedSize);
+	}
 
-    public ObservableMap<String, Predicate<S>> getFilterMap() {
-        return filterMap.get();
-    }
+	private MapProperty<String, Predicate<S>> filterMap = new SimpleMapProperty<>(FXCollections.observableMap(new TreeMap<String, Predicate<S>>()));
 
-    public MapProperty<String, Predicate<S>> filterMapProperty() {
-        return filterMap;
-    }
+	public ObservableMap<String, Predicate<S>> getFilterMap()
+	{
+		return filterMap.get();
+	}
 
-    protected void init() {
-        this.setRowFactory(new Callback<TableView<S>, TableRow<S>>() {
-            @Override
-            public TableRow<S> call(TableView<S> param) {
-                return new TableRow<S>() {
-                    @Override
-                    protected Skin<?> createDefaultSkin() {
-                        return new JFXTableRowSkin<>(this);
-                    }
-                };
-            }
-        });
-        this.fixedSize.addListener(o -> layout());
-        this.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        this.filterMap.addListener((MapChangeListener<String, Predicate<S>>) change -> {
-            if (filterMap.isEmpty()) this.setItems(itemSource);
-            else
-                filterMap.values().stream().reduce(Predicate::and)
-                        .ifPresent(p -> this.setItems(new FilteredList<>(this.itemSource, p)));
-        });
-    }
+	public MapProperty<String, Predicate<S>> filterMapProperty()
+	{
+		return filterMap;
+	}
+
+	protected void init()
+	{
+		this.setRowFactory(new Callback<TableView<S>, TableRow<S>>()
+		{
+			@Override
+			public TableRow<S> call(TableView<S> param)
+			{
+				return new TableRow<S>()
+				{
+					@Override
+					protected Skin<?> createDefaultSkin()
+					{
+						return new JFXTableRowSkin<>(this);
+					}
+				};
+			}
+		});
+		this.fixedSize.addListener(o -> layout());
+		this.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		this.filterMap.addListener((MapChangeListener<String, Predicate<S>>) change ->
+		{
+			if (!filterMap.isEmpty())
+				filterMap.values().stream().reduce(Predicate::and).ifPresent(itemSource::setPredicate);
+			this.getItems().setAll(itemSource);
+		});
+	}
 
 
-    @Override
-    protected double computeMinWidth(double height) {
-        if (!isFixedSize())
-            return super.computeMinWidth(height);
-        else {
-            double w = 0;
-            for (TableColumn<S, ?> column : this.getColumns())
-                w += column.getWidth() + 5;
-            return w;
-        }
-    }
+	@Override
+	protected double computeMinWidth(double height)
+	{
+		if (!isFixedSize())
+			return super.computeMinWidth(height);
+		else
+		{
+			double w = 0;
+			for (TableColumn<S, ?> column : this.getColumns())
+				w += column.getWidth() + 5;
+			return w;
+		}
+	}
 
-    private static EventHandler<MouseEvent> mouseDraggedHandler;
+	private static EventHandler<MouseEvent> mouseDraggedHandler;
 
-    static {
-        try {
-            Field field = TableColumnHeader.class.getDeclaredField("mouseDraggedHandler");
-            field.setAccessible(true);
-            mouseDraggedHandler = (EventHandler<MouseEvent>) field.get(null);
-        } catch (IllegalAccessException | NoSuchFieldException e) {
-            mouseDraggedHandler = null;
-            e.printStackTrace();
-        }
-    }
+	static
+	{
+		try
+		{
+			Field field = TableColumnHeader.class.getDeclaredField("mouseDraggedHandler");
+			field.setAccessible(true);
+			mouseDraggedHandler = (EventHandler<MouseEvent>) field.get(null);
+		}
+		catch (IllegalAccessException | NoSuchFieldException e)
+		{
+			mouseDraggedHandler = null;
+			e.printStackTrace();
+		}
+	}
 
-    public void expand() {
+	public void expand()
+	{
 //        ((TableViewSkin) getSkin()).createCell()
-    }
+	}
 
-    private DoubleProperty gap = new SimpleDoubleProperty(5);
+	private DoubleProperty gap = new SimpleDoubleProperty(5);
 
-    @Override
-    protected Skin<?> createDefaultSkin() {
-        return new TableViewSkin<S>(this) {
+	@Override
+	protected Skin<?> createDefaultSkin()
+	{
+		return new TableViewSkin<S>(this)
+		{
 
-            {
-                Callback<TableView<S>, TableRow<S>> callback = this.rowFactoryProperty().get();
-                this.rowFactoryProperty().set(param -> {
-                    TableRow<S> call = callback.call(param);
-                    call.setOnMouseEntered(e -> JFXDepthManager.setDepth(call, 3));
-                    call.setOnMouseExited(e -> JFXDepthManager.setDepth(call, 0));
-                    return call;
-                });
+			{
+				Callback<TableView<S>, TableRow<S>> callback = this.rowFactoryProperty().get();
+				this.rowFactoryProperty().set(param ->
+				{
+					TableRow<S> call = callback.call(param);
+					call.setOnMouseEntered(e -> JFXDepthManager.setDepth(call, 3));
+					call.setOnMouseExited(e -> JFXDepthManager.setDepth(call, 0));
+					return call;
+				});
 //                this.flow.setBackground(new Background(new BackgroundFill(Color.GREEN, new CornerRadii(5), new Insets(3))));
-            }
+			}
 
-            @Override
-            protected TableHeaderRow createTableHeaderRow() {
-                TableViewSkin<S> skin = this;
-                return new TableHeaderRow(this) {
+			@Override
+			protected TableHeaderRow createTableHeaderRow()
+			{
+				TableViewSkin<S> skin = this;
+				return new TableHeaderRow(this)
+				{
 
 
-                    protected NestedTableColumnHeader createRootHeader() {
-                        return new NestedTableColumnHeader(skin, null) {
+					protected NestedTableColumnHeader createRootHeader()
+					{
+						return new NestedTableColumnHeader(skin, null)
+						{
 
-                            protected TableColumnHeader createTableColumnHeader(TableColumnBase col) {
-                                return new JFXTableColumnHeader(getTableViewSkin(), col) {
-                                    {
-                                        this.onMouseDraggedProperty().bind(
-                                                Bindings.createObjectBinding(() ->
-                                                                isColumnsDraggable() ? mouseDraggedHandler : null,
-                                                        columnsDraggable));
-                                    }
-                                };
-                            }
-                        };
-                    }
-                };
-            }
-        };
-    }
+							protected TableColumnHeader createTableColumnHeader(TableColumnBase col)
+							{
+								return new JFXTableColumnHeader(getTableViewSkin(), col)
+								{
+									{
+										this.onMouseDraggedProperty().bind(
+												Bindings.createObjectBinding(() ->
+																isColumnsDraggable() ? mouseDraggedHandler : null,
+														columnsDraggable));
+									}
+								};
+							}
+						};
+					}
+				};
+			}
+		};
+	}
 }
